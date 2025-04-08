0aa0bc10b57cac596e39773eccbe8ae710659855
KarimAbushbak <karimabushbak@gmail.com>
2025-04-08 12:28:38 +0200

    first commit

---
+++ lib/core/resources/manager_assets.dart
@@ -0,0 +1,18 @@
+class ManagerAssets {
+  static const String splash = 'assets/images/splash/splash.png';
+  static const String outBoarding1 = 'assets/images/out_boarding/out_boarding1.png';
+  static const String outBoarding2 = 'assets/images/out_boarding/out_boarding2.png';
+  static const String outBoarding3 = 'assets/images/out_boarding/out_boarding3.png';
+  static const String facebook = 'assets/images/auth/facebook.png';
+  static const String google = 'assets/images/auth/google.png';
+  static const String home1 = 'assets/images/home/home1.png';
+  static const String home1White = 'assets/images/home/hom1_white.png';
+  static const String home2 = 'assets/images/home/home2.png';
+  static const String home3 = 'assets/images/home/home3.png';
+  static const String home4 = 'assets/images/home/home4.png';
+  static const String maleProfile = 'assets/images/profile/profile1.png';
+  static const String femaleProfile = 'assets/images/profile/profile2.png';
+}
+class ManagerPaths {
+  static const String translationsPath = 'assets/translations';
+}
